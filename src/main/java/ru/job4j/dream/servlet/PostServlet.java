bc740db6dc7ecf2cc3a4173feb4189641e794414
Mikita Mironenka <m.mironenka@gmail.com>
2020-09-02 21:17:54 +0300

    0. MVC в Servlet.[6868#317130]

--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -12,6 +12,12 @@ import java.io.IOException;
 public class PostServlet extends HttpServlet {
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("posts", Store.instOf().findAllPosts());
+        req.getRequestDispatcher("/post/posts.jsp").forward(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
         Store.instOf().save(
@@ -20,6 +26,6 @@ public class PostServlet extends HttpServlet {
                 req.getParameter("name")
             )
         );
-        resp.sendRedirect(req.getContextPath() + "/post/posts.jsp");
+        resp.sendRedirect(req.getContextPath() + "/posts.do");
     }
 }
