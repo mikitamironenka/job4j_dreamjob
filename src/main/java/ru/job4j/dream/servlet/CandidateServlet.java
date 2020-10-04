c1a9c251b2ae0445f4238cab86a651b19a058cf9
Mikita Mironenka <m.mironenka@gmail.com>
2020-10-04 16:41:56 +0300

    1. HttpSession[6869#317127]

---
+++ src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
@@ -0,0 +1,32 @@
+package ru.job4j.dream.servlet;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.store.PsqlStore;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CandidateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("candidates", PsqlStore.instOf().findAllCandidates());
+        req.setAttribute("user", req.getSession().getAttribute("user"));
+        req.getRequestDispatcher("/candidate/candidates.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.setCharacterEncoding("UTF-8");
+        PsqlStore.instOf().saveCandidate(
+            new Candidate(
+                Integer.valueOf(req.getParameter("id")),
+                req.getParameter("name"),
+                req.getIntHeader("photoId")
+            )
+        );
+        resp.sendRedirect(req.getContextPath() + "/candidates.do");
+    }
+}
